a3e96c0af4bb003b5cf65cf6201b8ef077e86ef7
Xiaodong Li <1128xiaodong@gmail.com>
2022-08-09 13:16:15 -0700

    update javadoc

--- GameTrigger.java
+++ GameTrigger.java
@@ -1,7 +1,12 @@
-package finalProject;
+/**
+ * CS5004 Summer 2022 Final Project - GAME 2048
+ * This class triggers a new game;
+ * @author Qian Lang; Xiaodong Li
+ * @version 0.0
+ * 09 Aug 2022
+ */
 
 public class GameTrigger {
-
     public static final GameWindow GAME_WINDOW = new GameWindow();
     public static final UserAction USER_ACTION = new UserAction();
     public static final GameLogic GAME_LOGIC = new GameLogic();
